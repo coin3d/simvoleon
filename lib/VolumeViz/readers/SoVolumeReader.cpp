97b47457aea320236a32ea7465d3a833d66586d5
Morten Eriksen <mortene@sim.no>
2002-11-09 12:52:01 +0000

    Implements fileSize() and parts of other methods.

--- lib/VolumeViz/readers/SoVolumeReader.cpp
+++ lib/VolumeViz/readers/SoVolumeReader.cpp
@@ -5,17 +5,25 @@
 */
 
 #include <VolumeViz/readers/SoVolumeReader.h>
+#include <Inventor/errors/SoDebugError.h>
 
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif // HAVE_CONFIG_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 class SoVolumeReaderP{
 public:
   SoVolumeReaderP(SoVolumeReader * master) {
     this->master = master;
   }
+
+  SbString filename;
 
 private:
   SoVolumeReader * master;
@@ -30,19 +38,63 @@ private:
 SoVolumeReader::SoVolumeReader(void)
 {
   PRIVATE(this) = new SoVolumeReaderP(this);
-
+  this->m_data = NULL;
 }
 
 SoVolumeReader::~SoVolumeReader()
 {
+  // FIXME: dealloc this->m_data when we allocated it
+  // ourselves. 20021109 mortene.
+
   delete PRIVATE(this);
+}
+
+void
+SoVolumeReader::setUserData(void * data)
+{
+  // FIXME: unimplemented. 20021108 mortene.
+}
+
+// *************************************************************************
+
+int
+SoVolumeReader::setFilename(const char * filename)
+{
+  PRIVATE(this)->filename = filename;
+
+  printf("filesize: %d\n", (int)this->fileSize());
+
+//   assert(FALSE && "impl stops here");
+
+  // TGS doc doesn't say what this is supposed to return.
+  return 0;
+}
+
+int64_t
+SoVolumeReader::fileSize(void)
+{
+  struct stat buf;
+  const char * filename = PRIVATE(this)->filename.getString();
+
+  if (stat(filename, &buf) == 0) {
+    return buf.st_size;
+  }
+  else {
+    SoDebugError::post("SoVolumeReader::fileSize", "couldn't stat() '%s': %s",
+                       filename, strerror(errno));
+    return -1;
+  }
+}
+
+void *
+SoVolumeReader::getBuffer(int64_t offset, unsigned int size)
+{
+  return NULL;
 }
 
 // FIXME: unimplemented methods. 20021108 mortene.
 
-void SoVolumeReader::setUserData(void * data) { }
-int SoVolumeReader::setFilename(const char * filename) { return 0; }
-void * SoVolumeReader::getBuffer(int64_t offset, unsigned int size) { return NULL; }
 int SoVolumeReader::bytesToInt(unsigned char * ptr, int sizeBytes) { return 0x0000; }
 void SoVolumeReader::swapBytes(int * intPtr, int sizeBytes) { return; }
-int64_t SoVolumeReader::fileSize(void) { return 0; }
+
+// *************************************************************************
