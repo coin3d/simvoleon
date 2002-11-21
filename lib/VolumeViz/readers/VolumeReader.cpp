4f056c784293e69106336c36d9a5341425d9dfca
Morten Eriksen <mortene@sim.no>
2002-11-21 11:06:44 +0000

    Removes some old debugging cruft.

--- lib/VolumeViz/readers/VolumeReader.cpp
+++ lib/VolumeViz/readers/VolumeReader.cpp
@@ -62,11 +62,6 @@ SoVolumeReader::setFilename(const char * filename)
 {
   PRIVATE(this)->filename = filename;
 
-#if CVR_DEBUG && 1 // debug
-  SoDebugError::postInfo("SoVolumeReader::setFilename",
-                         "filesize: %d", (int)this->fileSize());
-#endif // debug
-
   // TGS doc doesn't say what this is supposed to return.
   return 0;
 }
