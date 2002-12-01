9da007c679ff50bc912262b9da873bd267d99c94
Morten Eriksen <mortene@sim.no>
2002-12-01 11:53:28 +0000

    Suggested strategy for exploring the texmem capability of the current context.

--- lib/VolumeViz/render/2D/TexManager.cpp
+++ lib/VolumeViz/render/2D/TexManager.cpp
@@ -12,3 +12,35 @@
     subpages' timestamps are "touched" (updated) whenever they're
     rendered.
 */
+
+// FIXME: suggestion found on comp.graphics.api.opengl on how to check
+// how much texture VRAM is available:
+//
+// [...]
+// Once you create texture(s), you can give it(them) a priority using
+// glPrioritizeTexture
+// (http://www.3dlabs.com/support/developer/GLmanpages/glprioritizetextures.htm)
+// 
+// this should tell the ICD to put textures with the highest priority
+// (and according to available texture memory on your hardware) into
+// the actual video ram on you sweet geforce (or whatever card you
+// happen to use:-).
+// 
+// and then, you can see if that texture(s) is really resident (stored
+// in vram) by calling glAreTexturesResident
+// (http://www.3dlabs.com/support/developer/GLmanpages/glaretexturesresident.htm)
+// 
+// So if you want to test how many textures you can cram into your
+// vram, you can just keep on creating new textures and setting their
+// priority to high (1) and for every new one you create see if it is
+// really stored in vram.  Keep in mind that you need to use a given
+// texture at least once before it can be "prioritized", put into
+// vram.
+// 
+// This is really really cool, because say you want to use 10 textures
+// interchangeably. If you put them in vram then calls to
+// glBindTexture will be ultra fricking fast, because the texture will
+// no longer have to go through the AGP bus.
+// [...]
+//
+// 20021130 mortene.
