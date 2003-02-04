53dedb536b8c5e34056f85bbe729096155fdcd38
Morten Eriksen <mortene@sim.no>
2003-02-04 15:31:46 +0000

    Interface to OpenGL's paletted textures.

--- lib/VolumeViz/misc/CLUT.cpp
+++ lib/VolumeViz/misc/CLUT.cpp
@@ -1,6 +1,9 @@
 // This datatype is a color lookup table.
+//
+// FIXME: handle other types of entries apart from 32-bit RGBA.
 
 #include <VolumeViz/misc/CvrCLUT.h>
+
 
 CvrCLUT::CvrCLUT(const unsigned int nrentries, uint8_t * rgba8bits)
 {
@@ -14,4 +17,22 @@ CvrCLUT::CvrCLUT(const unsigned int nrentries, uint8_t * rgba8bits)
 CvrCLUT::~CvrCLUT()
 {
   delete[] this->entries;
+}
+
+void
+CvrCLUT::activate(const cc_glglue * glw) const
+{
+  // FIXME: should only need to do this once somewhere else
+  glEnable(GL_COLOR_TABLE);
+
+  // FIXME: handle this->nrentries != 256
+  assert(this->nrentries == 256);
+
+  cc_glglue_glColorTable(glw,
+                         GL_TEXTURE_2D, /* target */
+                         GL_RGBA, /* GL internalformat */
+                         this->nrentries, /* nr of paletteentries */
+                         GL_RGBA, /* palette entry format */
+                         GL_UNSIGNED_BYTE, /* palette entry unit type */
+                         this->entries); /* data ptr */
 }
--- lib/VolumeViz/misc/CvrCLUT.h
+++ lib/VolumeViz/misc/CvrCLUT.h
@@ -2,11 +2,14 @@
 #define COIN_CVRCLUT_H
 
 #include <Inventor/system/inttypes.h>
+#include <Inventor/C/glue/gl.h>
 
 class CvrCLUT {
 public:
   CvrCLUT(const unsigned int nrentries, uint8_t * rgba8bits);
   ~CvrCLUT();
+
+  void activate(const cc_glglue * glw) const;
 
 private:
   unsigned int nrentries;
