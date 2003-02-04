cb324a43c63f49950c17261a0ca1d8c6153a6ffe
Morten Eriksen <mortene@sim.no>
2003-02-04 18:33:40 +0000

    Implements support for transparency thresholds.

--- lib/VolumeViz/misc/CLUT.cpp
+++ lib/VolumeViz/misc/CLUT.cpp
@@ -1,10 +1,9 @@
 // This datatype is a color lookup table.
-//
-// FIXME: handle other types of entries apart from 32-bit RGBA.
 
 #include <VolumeViz/misc/CvrCLUT.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <Inventor/SbBasic.h>
 
 
 CvrCLUT::CvrCLUT(const unsigned int nrcols, const uint8_t * rgba8bits)
@@ -17,6 +16,8 @@ CvrCLUT::CvrCLUT(const unsigned int nrcols, const uint8_t * rgba8bits)
   this->int_entries = new uint8_t[blocksize];
   (void)memcpy(this->int_entries, rgba8bits, blocksize * sizeof(uint8_t));
   // (yeah, yeah, I know sizeof(uint8_t) is always == 1)
+
+  this->commonConstructor();
 }
 
 // nrcomponents == 1: LUMINANCE
@@ -32,6 +33,22 @@ CvrCLUT::CvrCLUT(const unsigned int nrcols, const unsigned int nrcomponents,
   const int blocksize = this->nrentries * this->nrcomponents;
   this->flt_entries = new float[blocksize];
   (void)memcpy(this->int_entries, colormap, blocksize * sizeof(float));
+
+  this->commonConstructor();
+}
+
+void
+CvrCLUT::commonConstructor(void)
+{
+  this->transparencythresholds[0] = 0;
+  this->transparencythresholds[1] = this->nrentries - 1;
+
+  // This is the block of fully transparent color values used by the
+  // glColorSubTable() calls which implements the transparency
+  // thresholds. It will be dynamically grown according to invocations
+  // of setTransparencyThresholds().
+  this->transparentblock = NULL;
+  this->transparentblockentries = 0;
 }
 
 CvrCLUT::~CvrCLUT()
@@ -40,6 +57,30 @@ CvrCLUT::~CvrCLUT()
     delete[] this->int_entries;
   if (this->datatype == FLOATS)
     delete[] this->flt_entries;
+
+  delete[] this->transparentblock;
+}
+
+// Everything below "low" (but not including) and above "high" (but
+// not including), will be fully transparent.
+void
+CvrCLUT::setTransparencyThresholds(uint32_t low, uint32_t high)
+{
+  assert(low <= high);
+  assert(low < this->nrentries);
+  assert(high < this->nrentries);
+
+  this->transparencythresholds[0] = low;
+  this->transparencythresholds[1] = high;
+
+  const int max_empty_entries = SbMax(low, this->nrentries - high - 1);
+  if (this->transparentblockentries < max_empty_entries) {
+    delete[] this->transparentblock;
+    uint8_t * ptr = this->transparentblock =
+      new uint8_t[max_empty_entries * 4];
+    this->transparentblockentries = max_empty_entries;
+    for (int i=0; i < this->transparentblockentries * 4; i++) { *ptr++ = 0x00; }
+  } 
 }
 
 void
@@ -89,7 +130,37 @@ CvrCLUT::activate(const cc_glglue * glw) const
   assert(glGetError() == GL_NO_ERROR);
 
   // Sanity check.
-  int actualsize;
-  glGetColorTableParameteriv(GL_TEXTURE_2D, GL_COLOR_TABLE_WIDTH, &actualsize);
-  assert(actualsize == this->nrentries);
+  GLint actualsize;
+  cc_glglue_glGetColorTableParameteriv(glw, GL_TEXTURE_2D,
+                                       GL_COLOR_TABLE_WIDTH, &actualsize);
+  assert(actualsize == (GLint)this->nrentries);
+
+
+  // Set up transparent areas of the colortable.
+
+  uint32_t low = this->transparencythresholds[0];
+  uint32_t high = this->transparencythresholds[1];
+  uint32_t numtrailing = this->nrentries - high - 1;
+
+  if (low > 0) {
+    cc_glglue_glColorSubTable(glw,
+                              GL_TEXTURE_2D, /* target */
+                              0, /* start */
+                              low, /* count */
+                              GL_RGBA, /* palette entry format */
+                              GL_UNSIGNED_BYTE, /* palette entry unit type */
+                              this->transparentblock); /* data ptr */
+  }
+
+  if (high < (this->nrentries - 1)) {
+    cc_glglue_glColorSubTable(glw,
+                              GL_TEXTURE_2D, /* target */
+                              high + 1, /* start */
+                              numtrailing, /* count */
+                              GL_RGBA, /* palette entry format */
+                              GL_UNSIGNED_BYTE, /* palette entry unit type */
+                              this->transparentblock); /* data ptr */
+  }
+
+  assert(glGetError() == GL_NO_ERROR);
 }
--- lib/VolumeViz/misc/CvrCLUT.h
+++ lib/VolumeViz/misc/CvrCLUT.h
@@ -11,18 +11,25 @@ public:
           const float * colormap);
   ~CvrCLUT();
 
+  void setTransparencyThresholds(uint32_t low, uint32_t high);
+
   void activate(const cc_glglue * glw) const;
 
 private:
+  void commonConstructor(void);
+
   unsigned int nrentries;
   unsigned int nrcomponents;
 
   enum DataType { INTS, FLOATS } datatype;
-
   union {
     uint8_t * int_entries;
     float * flt_entries;
   };
+
+  uint32_t transparencythresholds[2];
+  uint8_t * transparentblock;
+  int transparentblockentries;
 };
 
 #endif // !COIN_CVRCLUT_H
