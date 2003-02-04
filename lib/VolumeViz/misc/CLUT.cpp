46969c40fa9c839e993fe7a068b168a6a61998d9
Morten Eriksen <mortene@sim.no>
2003-02-04 16:11:02 +0000

    Handle floating point CLUT specifications.

--- lib/VolumeViz/misc/CLUT.cpp
+++ lib/VolumeViz/misc/CLUT.cpp
@@ -3,20 +3,43 @@
 // FIXME: handle other types of entries apart from 32-bit RGBA.
 
 #include <VolumeViz/misc/CvrCLUT.h>
+#include <assert.h>
+#include <stdlib.h>
 
 
-CvrCLUT::CvrCLUT(const unsigned int nrentries, uint8_t * rgba8bits)
+CvrCLUT::CvrCLUT(const unsigned int nrcols, const uint8_t * rgba8bits)
 {
-  this->nrentries = nrentries;
+  this->nrentries = nrcols;
+  this->nrcomponents = 4;
+  this->datatype = INTS;
 
-  const int blocksize = nrentries * 4;
-  this->entries = new uint8_t[blocksize];
-  (void)memcpy(this->entries, rgba8bits, blocksize);
+  const int blocksize = this->nrentries * this->nrcomponents;
+  this->int_entries = new uint8_t[blocksize];
+  (void)memcpy(this->int_entries, rgba8bits, blocksize * sizeof(uint8_t));
+  // (yeah, yeah, I know sizeof(uint8_t) is always == 1)
+}
+
+// nrcomponents == 1: LUMINANCE
+// nrcomponents == 2: LUMINANCE + ALPHA
+// nrcomponents == 4: RGBA
+CvrCLUT::CvrCLUT(const unsigned int nrcols, const unsigned int nrcomponents,
+                 const float * colormap)
+{
+  this->nrentries = nrcols;
+  this->nrcomponents = nrcomponents;
+  this->datatype = FLOATS;
+
+  const int blocksize = this->nrentries * this->nrcomponents;
+  this->flt_entries = new float[blocksize];
+  (void)memcpy(this->int_entries, colormap, blocksize * sizeof(float));
 }
 
 CvrCLUT::~CvrCLUT()
 {
-  delete[] this->entries;
+  if (this->datatype == INTS)
+    delete[] this->int_entries;
+  if (this->datatype == FLOATS)
+    delete[] this->flt_entries;
 }
 
 void
@@ -28,11 +51,45 @@ CvrCLUT::activate(const cc_glglue * glw) const
   // FIXME: handle this->nrentries != 256
   assert(this->nrentries == 256);
 
+  void * dataptr = NULL;
+  GLenum format, type;
+
+  if (this->datatype == INTS) {
+    dataptr = this->int_entries;
+    type = GL_UNSIGNED_BYTE;
+    format = GL_RGBA;
+  }
+  else if (this->datatype == FLOATS) {
+    dataptr = this->flt_entries;
+    type = GL_FLOAT;
+    switch (this->nrcomponents) {
+    case 1: format = GL_LUMINANCE; break;
+    case 2: format = GL_LUMINANCE_ALPHA; break;
+    case 4: format = GL_RGBA; break;
+    default: assert(FALSE); break;
+    }
+  }
+  else assert(FALSE);
+
+  // FIXME: should probably set glColorTableParameter() on
+  // GL_COLOR_TABLE_SCALE and GL_COLOR_TABLE_BIAS.
+
+  // FIXME: should probably check if all is ok with by using
+  // PROXY_TEXTURE_2D first.
+
   cc_glglue_glColorTable(glw,
                          GL_TEXTURE_2D, /* target */
                          GL_RGBA, /* GL internalformat */
                          this->nrentries, /* nr of paletteentries */
-                         GL_RGBA, /* palette entry format */
-                         GL_UNSIGNED_BYTE, /* palette entry unit type */
-                         this->entries); /* data ptr */
+                         format, /* palette entry format */
+                         type, /* palette entry unit type */
+                         dataptr); /* data ptr */
+
+  // Sanity check.
+  assert(glGetError() == GL_NO_ERROR);
+
+  // Sanity check.
+  int actualsize;
+  glGetColorTableParameteriv(GL_TEXTURE_2D, GL_COLOR_TABLE_WIDTH, &actualsize);
+  assert(actualsize == this->nrentries);
 }
--- lib/VolumeViz/misc/CvrCLUT.h
+++ lib/VolumeViz/misc/CvrCLUT.h
@@ -6,14 +6,23 @@
 
 class CvrCLUT {
 public:
-  CvrCLUT(const unsigned int nrentries, uint8_t * rgba8bits);
+  CvrCLUT(const unsigned int nrcols, const uint8_t * rgba8bits);
+  CvrCLUT(const unsigned int nrcols, const unsigned int nrcomponents,
+          const float * colormap);
   ~CvrCLUT();
 
   void activate(const cc_glglue * glw) const;
 
 private:
   unsigned int nrentries;
-  uint8_t * entries;
+  unsigned int nrcomponents;
+
+  enum DataType { INTS, FLOATS } datatype;
+
+  union {
+    uint8_t * int_entries;
+    float * flt_entries;
+  };
 };
 
 #endif // !COIN_CVRCLUT_H
