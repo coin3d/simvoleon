7f7a2f3506881ef9702bd796bac0ccd6c7be4aa1
Morten Eriksen <mortene@sim.no>
2003-03-24 11:07:52 +0000

    Code comments.

--- lib/VolumeViz/readers/VolumeReader.cpp
+++ lib/VolumeViz/readers/VolumeReader.cpp
@@ -23,6 +23,56 @@
 #include <errno.h>
 #include <string.h>
 
+// FIXME: the following doc might be outdated. Audit and
+// edit. 20030324 mortene.
+
+/*
+VOLUMEREADERS
+
+  Currently, only a reader of memory provided data is implemented
+  (SoVRMemReader). SoVolumeData uses the interface specified with
+  SoVolumeReader, and extensions with other readers should be straight
+  forward. When running setReader or setVolumeData, only a pointer to
+  the reader is stored. In other words, things could go bananas if the
+  client application start mocking around with the reader's settings
+  after a call to setReader. If the reader is changed, setReader must
+  be run over again.  This requirement differs from TGS as their
+  implementation loads all data once specified a reader (I guess).
+
+  The TGS interface for SoVolumeReader contains a function getSubSlice
+  with the following definition:
+
+  void getSubSlice(SbBox2s &subSlice, int sliceNumber, void * data)
+
+  It returns a subpage within a specified page along the z-axis. This
+  means that the responsibility for building pages along X and Y-axis
+  lies within the reader-client.When generating textures along either
+  x- or y-axis, this requires a significant number of iterations, one
+  for each page along the z-axis. This will in turn trigger plenty
+  filereads at different disklocations, and your disk's heads will have
+  a disco showdown the Travolta way. I've extended the interface as
+  following:
+
+  void getSubSlice(SbBox2s &subSlice,
+                   int sliceNumber,
+                   void * data,
+                   SoOrthoSlice::Axis axis = SoOrthoSlice::Z)
+
+  This moves the responsibility for building pages to the reader.
+  It makes it possible to exploit fileformats with possible clever
+  data layout, and if the fileformat/input doesn't provide intelligent
+  organization, it still wouldn't be any slower. The only drawback is
+  that some functionality would be duplicated among several readers
+  and making them more complicated.
+
+  The consequences is that readers developed for TGS's implementation
+  would not work with ours, but the opposite should work just fine.
+
+  torbjorv 08292002
+*/
+
+// *************************************************************************
+
 class SoVolumeReaderP{
 public:
   SoVolumeReaderP(SoVolumeReader * master) {
