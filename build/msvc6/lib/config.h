55490e44ad80c2e670ee5d2b95f3b86d0d48abc5
Lars J. Aas <larsa@sim.no>
2005-03-31 16:01:45 +0000

    autogenerated files

--- build/msvc6/lib/config.h
+++ build/msvc6/lib/config.h
@@ -71,7 +71,7 @@
 #define SIMVOLEON_VERSION "2.0.2a"
 
 /* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
+/* #undef STDC_HEADERS */
 
 /* Version number of package */
 #define VERSION "2.0.2a"
