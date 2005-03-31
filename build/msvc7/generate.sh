dcaa6f4c5ccacdc61956f6b8f511c5366ac513cc
Lars J. Aas <larsa@sim.no>
2005-03-31 16:18:19 +0000

    updates/fixes

--- build/msvc7/generate.sh
+++ build/msvc7/generate.sh
@@ -1,27 +1,39 @@
 #! /bin/sh
 #
 # This script generates the Visual Studio 6 build files for Windows.
-# The MSVC7 conversion is currently done manually.
 #
 # 20041214 larsa
 
-rm -rf simvoleon2.dsp simvoleon2.dsw installsimvoleonheaders.bat
+rm -f simvoleon2.dsp simvoleon2.dsw installsimvoleonheaders.bat
 
-../../configure --enable-msvcdsp --with-msvcrt=mt
-make
+../../configure --enable-msvcdsp --with-msvcrt=mt || exit 1
+make || exit 1
 
 build_pwd=`pwd`
 build="`cygpath -w $build_pwd | sed -e 's/\\\\/\\\\\\\\/g'`"
+build_pwd="`pwd | sed -e 's/\\//\\\\\\\\/g'`\\\\"
+
 source_pwd=`cd ../..; pwd`
 source="`cygpath -w $source_pwd | sed -e 's/\\\\/\\\\\\\\/g'`"
+source_pwd="`(cd ../..; pwd) | sed -e 's/\\//\\\\\\\\/g'`"
 
 sed \
   -e "s/$build/./g" \
+  -e "s/$build_pwd//g" \
   -e "s/$source/..\\\\../g" \
+  -e "s/$source_pwd/..\\\\../g" \
   -e 's/$/\r/g' \
   <simvoleon2.dsp >new.dsp
+
 mv new.dsp simvoleon2.dsp
 
-# How can I avoid the modal upgrade prompt-dialog for MSVC7.1 here???
-# devenv /command "File.OpenProject $build\\coin2.dsp"
+sed \
+  -e "s/$build/./g" \
+  -e "s/$build_pwd//g" \
+  -e "s/$source/..\\\\../g" \
+  -e "s/$source_pwd/..\\\\../g" \
+  -e 's/$/\r/g' \
+  <installsimvoleonheaders.bat >new.bat
+
+mv new.bat installsimvoleonheaders.bat
 
